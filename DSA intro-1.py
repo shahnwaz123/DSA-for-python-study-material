cfdb21ffc50f39e77720085eb9d08de85d3cb9ae
shahnwaz khan <88053176+shahnwaz123@users.noreply.github.com>
2021-08-19 03:27:06 +0530

    Add files via upload

---
+++ DSA intro-1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction of DSA(Data structure algorithm)
+
+# In[1]:
+
+
+# Data structure are building block or raw material for any software programmer.
+
+
+# In[2]:
+
+
+# Types of data structure are-
+
+# 1. Array
+# 2. Link list
+# 3. Tree
+
+
+# In[3]:
+
+
+# images of data structure is in another file-
+
+
+# In[4]:
+
+
+# Example of two cases ist we implement list and second dictionary is importatnt for that data structure-
+
+
+# In[ ]:
+
+
+# Case-1
+
+
+# In[5]:
+
+
+# Q1- Store apple stock price and answer-
+
+#1. what was the prince on day 1.
+#2. what was the price on day 3.
+
+
+# In[ ]:
+
+
+stock_price=[298,305,320,350,292]stock_price[0]  #stock price on day one.
+stock_price[1]  # stock price on day two.
+
+
+# In[7]:
+
+
+# Case-2
+
+
+# In[9]:
+
+
+# Q2-Store apple stock price from march 4 to march 7 and answer.
+# 1. What was the price on march 7
+# 2. what was the price on march 3
+
+
+# In[ ]:
+
+
+stock_price={
+    'march 4':298,
+    'march 5':305,
+    'march 6':320,
+    'march 7':301
+}
+
+stock_price['march 7']
+stock_price['march 6']
+
+
+# In[ ]:
+
+
+
+
